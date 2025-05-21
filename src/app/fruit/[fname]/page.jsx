2cc44c5a24839394d29a73930e7aff2cdcaf1b1c
prashant-tlp <prashantsinghrajput56@gmail.com>
2025-05-21 16:11:04 +0530

    data change according to slug

--- src/app/fruit/[fname]/page.jsx
+++ src/app/fruit/[fname]/page.jsx
@@ -1,3 +1,5 @@
+import axios from "axios"
+import Link from "next/link";
 
 
 
@@ -6,10 +8,25 @@ export const metadata = {
     description: 'Learn all about mangoes and bananas.',
 }
 
-export default function page({ params }) {
+export default async function page({ params }) {
+    const res = await axios.get(`https://dummyjson.com/recipes/${params.fname}`);
+    const data = res?.data
     return (
         <>
-            {params.fname}
+            <div>
+                <p>{data.name}</p>
+                <div>
+                    <p>Instructions to made</p>
+                    {
+                        data.instructions.map((ins,i)=>{
+                            return(
+                                <p key={ins}>{ins}</p>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <Link className="text-red-200 border border-red-400 p-3 " href={'/home'}>Home</Link>
         </>
     )
 }
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,12 +1,31 @@
-import Link from "next/link"
+import Link from "next/link";
+import axios from "axios";
+import Image from "next/image";
 
-export default async function () {
+export default async function PhotoGallery() {
+    const res = await axios.get("https://dummyjson.com/recipes");
+    const photos = res.data.recipes.slice(0, 10);
 
     return (
         <>
-        Home
-
-            <Link href={'/about'}>About</Link>
+            <div>
+                <Link href="/about">About</Link>
+            </div>
+            <div className="grid grid-cols-6 ">
+                {photos.map((e, i) => (
+                    <div key={e.id}>
+                        <p>{i + 1}</p>
+                        <Link href={`/fruit/${e.id}`}>
+                            <Image
+                                height={200}
+                                width={200}
+                                src={e.image}
+                                alt={e.name}
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </>
-    )
+    );
 }
